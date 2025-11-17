4d29552c5d718cc5f393f646b8ccef388232e8eb
Gerd Grimmen <gerd.grimmen.gg@gmail.com>
2025-11-17 22:57:12 +0100

    maybe structure

---
+++ main.py
@@ -0,0 +1,22 @@
+# verion 0.0.0
+# proto-flow
+
+flow = {
+    "steps" = {
+        "1": {"keys": {
+
+        }, "conditions": {
+
+        }},
+        "2": {},
+    }
+}
+
+keys = {}
+
+conditions = {}
+
+
+if __name__ == "__main__":
+    pas
+    
